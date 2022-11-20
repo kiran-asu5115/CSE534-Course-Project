a12989f809407a69a7cc40bbcd78787026e90d61
ShikharG <shikharprakhar@gmail.com>
2022-11-20 02:42:28 -0700

    Added network generator

--- lib/network_generator.py
+++ lib/network_generator.py
@@ -1,100 +1,53 @@
+import json
+
 from scapy.all import *
 from scapy.layers.inet import IP, TCP, UDP, ICMP
-import socket
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
+
+def gen_tcp_packet(dest, count=2000, iface="ens8"):
+    ans, unans = sr(IP(dst=dest) / TCP(dport=[3000, 5000, 6000]), inter=0.1, retry=count, timeout=1, iface=iface)
+    return ans, unans
+
+
+def gen_udp_packet(dest, count=2000, iface="ens8"):
+    ans, unans = sr(IP(dst=dest) / UDP(dport=[3000, 5000, 6000]), inter=0.1, retry=count, timeout=1, iface=iface)
+    return ans, unans
+
+
+def gen_icmp_packet(dest, count=2000, iface="ens8"):
+    ans, unans = sr(IP(dst=dest) / ICMP(), inter=0.1, retry=count, timeout=1, iface=iface)
+    return ans, unans
+
+
+def sniff_packet(packet_type, intf):
+    return True
+
+
 @app.route("/send_traffic", methods=["POST"])
 def update_content():
-
-    return jsonify({'packets_sent': ""})
-@app.route("/send_junk_traffic", methods=["POST"])
-def update_content():
-
-    return jsonify({'packets_sent': ""})
-
-class NetworkGen:
-    def __init__(self, client_port=5081, server_port=5080):
-        self.server_port = server_port
-        pass
-
-    def gen_tcp_packet(self, dest, count=2000, iface="ens8"):
-        ans, unans = sr(IP(dst=dest) / TCP(dport=[3000, 5000, 6000]), inter=0.1, retry=count, timeout=1, iface=iface)
-        print(ans, unans)
-
-    def gen_udp_packet(self, dest, count=2000, iface="ens8"):
-        ans, unans = sr(IP(dst=dest) / UDP(dport=[3000, 5000, 6000]), inter=0.1, retry=count, timeout=1, iface=iface)
-        print(ans, unans)
-
-    def gen_icmp_packet(self, dest, count=2000, iface="ens8"):
-        ans, unans = sr(IP(dst=dest) / ICMP(), inter=0.1, retry=count, timeout=1, iface=iface)
-        print(ans, unans)
-
-    def server(self, server_ip):
-        # take the server name and port name
-        host = server_ip
-        port = 5000
-
-        # create a socket at server side
-        # using TCP / IP protocol
-        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
-        # bind the socket with server
-        # and port number
-        s.bind(('', port))
-
-        # allow maximum 1 connection to
-        # the socket
-        s.listen(1)
-
-        # wait till a client accept
-        # connection
-        c, addr = s.accept()
-
-        # display client address
-        print("CONNECTION FROM:", str(addr))
-
-        # send message to the client after
-        # encoding into binary string
-        c.send(b"Reply from server")
-
-        msg = "Bye.............."
-        c.send(msg.encode())
-
-        # disconnect the server
-        c.close()
-
-    def client(self,server_ip):
-        # take the server name and port name
-        host = server_ip
-        port = 5000
-
-        # create a socket at client side
-        # using TCP / IP protocol
-        s = socket.socket(socket.AF_INET,
-                          socket.SOCK_STREAM)
-
-        # connect it to server and port
-        # number on local computer.
-        s.connect((host, port))
-
-        # receive message string from
-        # server, at a time 1024 B
-        msg = s.recv(1024)
-
-        # repeat as long as message
-        # string are not empty
-        while msg:
-            print('Received:' + msg.decode())
-            msg = s.recv(1024)
-
-        # disconnect the client
-        s.close()
-    def start_server(self):
-
-        app.config['SECRET_KEY'] = 'secret_101$'
-        app.run(host="0.0.0.0", port=self.server_port, debug=True)
+    packet_type = request.args.get('packet_type', None)
+    dest = request.args.get('dest', None)
+    count = int(request.args.get('count'))
+    if packet_type == "ICMP":
+        gen_icmp_packet(dest, count)
+    elif packet_type == "UDP":
+        gen_udp_packet(dest, count)
+    else:
+        gen_tcp_packet(dest, count)
+    return json.dumps({'success': True}), 200, {'ContentType': 'application/json'}
 
 
-n = NetworkGen()
+@app.route("/sniff_packet", method=["POST"])
+def sniff():
+    packet_type = request.args.get('packet_type', None)
+    intf = request.args.get('intf')
+    sniffed_packetss = sniff_packet(packet_type, intf)
+    return json.dumps({'success': True, "packets": sniffed_packetss}), 200, {'ContentType': 'application/json'}
+
+
+if "__name__" == "main":
+    app.config['SECRET_KEY'] = 'secret_101$'
+    app.run(host="0.0.0.0", port=5080, debug=True)
--- main.py
+++ main.py
@@ -1,7 +1,14 @@
 import os
+import random
 import sys
 import json
+
+import requests
+
 from config_paths import get_lib_config_path, get_project_config_path, get_slice_config_path
+from flask import Flask, jsonify, request, render_template
+
+app = Flask(__name__)
 
 lib_config_path = get_lib_config_path()
 sys.path.insert(0, lib_config_path)
@@ -13,7 +20,8 @@ def set_environ(filename):
     for key, value in data.items():
         os.environ[key] = value
 
-project_config_path = get_project_config_path() 
+
+project_config_path = get_project_config_path()
 set_environ(project_config_path)
 
 from lib.slice_builder import MeasurementSlice
@@ -26,20 +34,57 @@ def read_config(filename):
     return data
 
 
+@app.route('/')
+def home():
+    return render_template("home.html")
+
+
+@app.route('/send_traffic', method=["POST", "GET"])
+def home():
+    packet_type = request.form.get('packet_type')
+    source = request.form.get('source')
+    dest = request.form.get('dest')
+    count = request.form.get('count')
+    res = requests.get("http://%s:5080/send_traffic" % source, data={"packet_type": packet_type,
+                                                                     "dest": dest,
+                                                                     "count": count})
+    return render_template("home.html")
+
+
+@app.route('/send_junk_traffic', method=["POST", "GET"])
+def home():
+    packet_type = request.form.get('packet_type')
+    source = request.form.get('source')
+    dest = "192.168.1.%s" % random.randint(5,50)
+    count = request.form.get('count')
+    res = requests.get("http://%s:5080/send_traffic" % source, data={"packet_type": packet_type,
+                                                                     "dest": dest,
+                                                                     "count": count})
+    return render_template("home.html")
+
+@app.route('/sniff', method=["POST", "GET"])
+def home():
+    packet_type = request.form.get('packet_type')
+    source = request.form.get('source')
+    intf = request.form.get('intf')
+    res = requests.get("http://%s:5080/send_traffic" % source, data={"packet_type": packet_type,
+                                                                     "intf": intf})
+    return render_template("home.html")
+
 def main():
     slice_config_file_name = "final_topo_slice_config.json"
     slice_config_path = get_slice_config_path(file_name=slice_config_file_name)
     slice_config = read_config(slice_config_path)
-    # print(slice_config)
     s = MeasurementSlice(slice_config)
-    # s.create_slice()
-    # s.get_slice_ssh_commands()
+    s.create_slice()
+    s.get_slice_ssh_commands()
     s.configure_ips()
-    # s.add_config_files_to_slice_nodes()
+    s.add_config_files_to_slice_nodes()
 
     inst_slice = Instrumentize(slice_config["slice_name"])
+    app.config['SECRET_KEY'] = 'secret_101$'
+    app.run(host="0.0.0.0", port=5081, debug=True)
 
-    
+
 if __name__ == '__main__':
     main()
-
